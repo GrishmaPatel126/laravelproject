5ad8e64247be240ed1777da27bb924a7311e859b
Grishma Patel <patelgrishma126@gmail.com>
2022-05-31 16:44:58 +0530

    first commit

---
+++ app/Http/Controllers/QuarryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quarry;
+use Illuminate\Http\Request;
+
+class QuarryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Quarry  $quarry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quarry $quarry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Quarry  $quarry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Quarry $quarry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Quarry  $quarry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Quarry $quarry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Quarry  $quarry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quarry $quarry)
+    {
+        //
+    }
+}
